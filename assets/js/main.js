5884227334bfacf4c2cc10e69a70e54a6b317930
unknown <oswaldordonez@unicauca.edu.co>
2021-07-28 17:39:59 -0500

    Services and Portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,10 +38,49 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SERVICES MODAL ====================*/
 
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns = document.querySelectorAll('.services__button'),
+      modalCloses = document.querySelectorAll('.services__modal-close');
 
+let modal = function(modalClick){
+     modalViews[modalClick].classList.add('active-modal')
+} 
+let closeModal = function(modalClick){
+    modalViews[modalClick].classList.remove('active-modal')
+} 
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose, i) => {
+    modalClose.addEventListener('click', () =>{
+        closeModal(i)
+    })
+})
 /*==================== PORTFOLIO SWIPER  ====================*/
 
-
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.98,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
 /*==================== TESTIMONIAL ====================*/
 
 
